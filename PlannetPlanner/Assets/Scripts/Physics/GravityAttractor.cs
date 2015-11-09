8588f503c1dc1cc1a8d7892255ef5ef80e645705
CalebOuellette <caleb.ouellette@gmail.com>
2015-11-09 11:18:23 -0800

    Added Trigger Distance for Planets

--- PlannetPlanner/Assets/Scripts/Physics/GravityAttractor.cs
+++ PlannetPlanner/Assets/Scripts/Physics/GravityAttractor.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class GravityAttractor : MonoBehaviour {
 
 	public float gravity = -12;
-    private Transform planet;
+	public float triggerDistance = 10;	
     
+	private Transform planet;
+
    public void Awake ()
     {
         planet = GetComponent<Transform>();
@@ -13,13 +15,20 @@ public class GravityAttractor : MonoBehaviour {
 
 
     public Vector2 Attract(Transform body) {
-		Vector2 gravityUp = (body.position - planet.position).normalized;
-      
 
-        gravityUp = (gravityUp * gravity);
+	
+		Vector2 gravityUp = (body.position - planet.position); //Get Vector between two points
+
+		if (gravityUp.magnitude < triggerDistance) { //if the magnitude(distance) is less then trigger Distance, Apply gravity
 
 
-        return gravityUp;
+			gravityUp = (gravityUp.normalized * gravity);
+			return gravityUp;
+		} else { //Else return a 0 vector. Equal to no force added.
+			Vector2 x = new Vector2(0,0);
+			return x;
+		
+		}
 
     }
 
