c90b4aa0248385e3d96ce6d4b6bb36b713b14309
CalebOuellette <Caleb.Ouellette@gmail.com>
2015-11-08 22:58:23 -0800

    Starting to add boost

---
+++ PlannetPlanner/Assets/Scripts/Physics/boost.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boost : MonoBehaviour {
+
+    public bool[] Boost = new bool [240];
+
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        aTimer = new System.Timers.Timer();
+        while ()
+        {
+
+
+
+        }
+	}
+
+}
