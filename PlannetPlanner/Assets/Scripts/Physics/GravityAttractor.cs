627c4b42e88fdc91db7fefd8a94a25dccdb111d9
CalebOuellette <caleb.ouellette@gmail.com>
2015-11-09 16:13:13 -0800

    Added Trigger distance

--- PlannetPlanner/Assets/Scripts/Physics/GravityAttractor.cs
+++ PlannetPlanner/Assets/Scripts/Physics/GravityAttractor.cs
@@ -11,6 +11,19 @@ public class GravityAttractor : MonoBehaviour {
    public void Awake ()
     {
         planet = GetComponent<Transform>();
+		if (triggerDistance != -1) {
+
+			GameObject line = Instantiate(Resources.Load("Line", typeof(GameObject)), planet.position, planet.rotation) as GameObject;
+			float scale = (triggerDistance * 2 )/ 10; //fix
+			line.transform.parent = planet;
+
+			line.transform.localScale = new Vector3(scale , scale, scale );
+
+
+
+		}
+
+
     }
 
 
@@ -19,7 +32,7 @@ public class GravityAttractor : MonoBehaviour {
 	
 		Vector2 gravityUp = (body.position - planet.position); //Get Vector between two points
 
-		if (gravityUp.magnitude < triggerDistance) { //if the magnitude(distance) is less then trigger Distance, Apply gravity
+		if (gravityUp.magnitude < triggerDistance || triggerDistance == -1) { //if the magnitude(distance) is less then trigger Distance, Apply gravity
 
 
 			gravityUp = (gravityUp.normalized * gravity);
