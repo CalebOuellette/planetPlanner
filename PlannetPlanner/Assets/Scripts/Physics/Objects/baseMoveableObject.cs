139e0c9c0267d8923c6e54ca2d5d6ef3ff78d392
CalebOuellette <caleb.ouellette@gmail.com>
2015-11-18 12:47:12 -0800

    Added OO base

---
+++ PlannetPlanner/Assets/Scripts/Physics/Objects/baseMoveableObject.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class baseMoveableObject : MonoBehaviour {
+
+	//requires 2D rigid body
+
+	private Vector2 startPosition;
+	private Vector2 playerMovedStartPosition;
+
+
+	// Use this for initialization
+	void Start () {
+		startPosition = GetComponent<Rigidbody2D>().position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	// Set postion to reset back to.
+	public void setPlayerMovedStartPosition (){
+		playerMovedStartPosition = GetComponent<Rigidbody2D>().position;
+	}
+
+	//Reset object to level start postion
+	public void resetToStart(){
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		rb.position = startPosition; 
+	}
+
+	//Reset object to player moved start postion
+	public void resetToPMstartPosition (){
+		Rigidbody2D rb = GetComponent<Rigidbody2D>();
+		rb.position = playerMovedStartPosition; 
+	}
+
+}
