9d528be2259d5bc414b11dfbe1b4ad3291606db6
CalebOuellette <caleb.ouellette@gmail.com>
2015-11-20 14:12:38 -0800

    Added goal scripts

---
+++ PlannetPlanner/Assets/Scripts/Physics/onContact/levelCompleteOnContact.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class levelCompleteOnContact : MonoBehaviour
+{
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		Debug.Log("collision");
+		if (col.gameObject.name == "spaceship")
+		{
+
+			//Add victory event
+
+
+			
+		}
+	}
+}
