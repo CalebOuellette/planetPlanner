4b913208c58b22d89886fd34b31d7abe5a5ce2e9
CalebOuellette <Caleb.Ouellette@gmail.com>
2015-12-30 20:28:23 -0800

    UI update

--- PlanetPlanner/Assets/Scripts/Control/PlayerController.cs
+++ PlanetPlanner/Assets/Scripts/Control/PlayerController.cs
@@ -1,81 +1,100 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
 
     public int thrust = 10;
-	public bool UseArrowControls = false;
+    public bool UseArrowControls = false;
     private Rigidbody2D rb;
     private Transform body;
-	private levelClock levelClock;
+    private levelClock levelClock;
+    private GameObject spaceship;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        body = GetComponent<Transform>();
-        levelClock = GameObject.Find("gameBaseScripts").GetComponent<levelClock>(); 
+
+        levelClock = GameObject.Find("gameBaseScripts").GetComponent<levelClock>();
+        spaceship = GameObject.Find("spaceship");
+        rb = spaceship.GetComponent<Rigidbody2D>();
+        body = spaceship.GetComponent<Transform>();
 
     }
 
 
 
-	void Update() {
-		if (UseArrowControls == true) {
-			if (Input.GetKey (KeyCode.RightArrow)) {
-				rb.AddForce (Vector2.right * thrust);
-				Debug.Log ("right");
+    void Update()
+    {
+        if (UseArrowControls == true)
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                rb.AddForce(Vector2.right * thrust);
+                Debug.Log("right");
 
-			}
-			if (Input.GetKey (KeyCode.LeftArrow)) {
-				rb.AddForce (Vector2.left * thrust);
-				Debug.Log ("left");
-			}
-			if (Input.GetKey (KeyCode.DownArrow)) {
-				rb.AddForce (Vector2.down * thrust);
-				Debug.Log ("down");
-			}
-			if (Input.GetKey (KeyCode.UpArrow)) {
-				rb.AddForce (Vector2.up * thrust);
-				Debug.Log ("up");
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.AddForce(Vector2.left * thrust);
+                Debug.Log("left");
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                rb.AddForce(Vector2.down * thrust);
+                Debug.Log("down");
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb.AddForce(Vector2.up * thrust);
+                Debug.Log("up");
 
-			}
+            }
 
-			if (Input.GetKey (KeyCode.B)) {            
-				rb.AddForce (body.right * thrust);
-				Debug.Log ("Boost");
+            if (Input.GetKey(KeyCode.B))
+            {
+                rb.AddForce(body.right * thrust);
+                Debug.Log("Boost");
 
-			}
-		}
-   
+            }
+        }
+
 
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-			reset();
+            reset();
         }
 
 
-}
+    }
 
 
-	public void reset(){
-		if (levelClock.started == false)
-		{
-			baseMoveableObject.startLevelAll();
-			
-		}
-		else
-		{
-			baseMoveableObject.resetAll();
-			
-		}
+    public void reset()
+    {
+
+        
+        if (levelClock.started == false)
+        {
+            baseMoveableObject.startLevelAll();
+
+        }
+        else
+        {
+            baseMoveableObject.resetAll();
+
+        }
 
 
 
 
+    }
+
+    public void spawnPlanet()
+    {
 
 
-	}
+
+    }
 
 }
