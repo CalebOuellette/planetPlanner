b7b894ed7ae9413148cd1c7c4285c0c18e5142c0
CalebOuellette <Caleb.Ouellette@gmail.com>
2016-01-19 20:06:00 -0800

    PinchTOOOOZOOOMMM

---
+++ PlanetPlanner/Assets/Scripts/Control/PinchZoom.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class PinchZoom : MonoBehaviour
+{
+    
+    public float orthoZoomSpeed = 0.5f;        // The rate of change of the orthographic size in orthographic mode.
+
+
+    void Update()
+    {
+        // If there are two touches on the device...
+        if (Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            Camera camera = GetComponent<Camera>();
+
+            // If the camera is orthographic...
+
+            // ... change the orthographic size based on the change in distance between the touches.
+            camera.orthographicSize += deltaMagnitudeDiff * orthoZoomSpeed;
+
+            // Make sure the orthographic size never drops below zero.
+            camera.orthographicSize = Mathf.Max(camera.orthographicSize, 0.1f);
+            
+      
+        }
+    }
+}
